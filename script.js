067456e6b7e7f4daa0b63b02c4c89cab0c7a11bb
Ysaline <ysaline.franquet@gmail.com>
2022-12-12 14:31:34 +0100

    Creating Player and Game objects
    Completed the player and game classes in the script.js file

--- script.js
+++ script.js
@@ -20,7 +20,26 @@ window.addEventListener('load', function(){
     }
     // Control the main character (animate player)
     class Player {
+        // constructor() creates a new JS object and assign it properties and values based on the blueprint inside
+        constructor(game){
+            this.game = game;
+            this.width = 120;
+            this.height = 190;
+            // player's x position
+            this.x = 20;
+            // player's y position
+            this.y = 100;
+            this.speedY = 0;
+        }
+        // method to move player around
+        update(){
+            this.y += this.speedY;
+        }
 
+        // draw graphics representing the player
+        draw(context) {
+            context.fillRect(this.x, this.y, this.width, this.height);
+        }
     }
     // Handle different enemy types
     class Enemy {
@@ -40,6 +59,20 @@ window.addEventListener('load', function(){
     }
     // Handle the entire game
     class Game {
-
+        constructor(width, height){
+            this.width = width;
+            this.height = height;
+            // new Player(this) ==> this refer to the class Game
+            this.player = new Player(this);
+        }
+        update(){
+            this.player.update();
+        }
+        draw(context){
+            // Render the player on the canvas by calling the draw method from line 40
+            this.player.draw(context);
+        }
     }
+
+    const game = new Game(canvas.width, canvas.height);
 });
