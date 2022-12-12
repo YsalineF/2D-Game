239cd07f9ed58bf087136801a9eae4cc1be5121b
Ysaline <ysaline.franquet@gmail.com>
2022-12-12 17:16:32 +0100

    Keyboard Inputs
    The player can now be moved up and down by pressing up and down arrow keys

--- script.js
+++ script.js
@@ -8,7 +8,33 @@ window.addEventListener('load', function(){
 
     // Keep track of specified user inputs (ex: arrow keys)
     class InputHandler {
-
+        constructor(game) {
+            this.game = game;
+            // When we press the key
+            window.addEventListener('keydown', e => {
+                // Check if arrowUp or ArrowDown is pressed and if the key is not yet in the array (if not present, it will return -1)
+                if((
+                    (e.key === "ArrowUp") ||
+                    (e.key === "ArrowDown")
+                ) && this.game.keys.indexOf(e.key) === -1){
+                    this.game.keys.push(e.key);
+                }
+                console.log(this.game.keys);
+            });
+            // When we release the key
+            window.addEventListener('keyup', e => {
+                // the .indexOf() method returns the first index at which a given element can be found in the array
+                // if it's not present, it will return -1
+                if(this.game.keys.indexOf(e.key) > -1){
+                    // the .splice() method changes the content of an array by removing or replacing existing elements
+                    // it needs at least 2 arguments : an index (where we want to start changing the array) and a delete
+                    // count (an int indicating the number of elements in the array we want to remove from that 
+                    // starting index)
+                    this.game.keys.splice(this.game.keys.indexOf(e.key), 1);
+                }
+                console.log(this.game.keys);
+            })
+        }
     }
     // Handle player's lasers
     class Projectile {
@@ -30,9 +56,13 @@ window.addEventListener('load', function(){
             // player's y position
             this.y = 100;
             this.speedY = 0;
+            this.maxSpeed = 3;
         }
         // method to move player around
         update(){
+            if(this.game.keys.includes('ArrowUp')) this.speedY = -this.maxSpeed;
+            else if(this.game.keys.includes('ArrowDown')) this.speedY = this.maxSpeed;
+            else this.speedY = 0;
             this.y += this.speedY;
         }
 
@@ -64,6 +94,8 @@ window.addEventListener('load', function(){
             this.height = height;
             // new Player(this) ==> this refer to the class Game
             this.player = new Player(this);
+            this.input = new InputHandler(this);
+            this.keys = [];
         }
         update(){
             this.player.update();
