7795927639fdc835256a242c0fe7df7e1a7aec2b
Ysaline <ysaline.franquet@gmail.com>
2022-12-12 14:18:33 +0100

    Object Oriented Programming in JS
    Created the different classes we are going to need to code the game

--- script.js
+++ script.js
@@ -5,4 +5,41 @@ window.addEventListener('load', function(){
     const ctx = canvas.getContext('2d');
     canvas.width = 500;
     canvas.height = 500;
+
+    // Keep track of specified user inputs (ex: arrow keys)
+    class InputHandler {
+
+    }
+    // Handle player's lasers
+    class Projectile {
+
+    }
+    // Handle falling screws and bolts (from damaged enemies)
+    class Particle {
+
+    }
+    // Control the main character (animate player)
+    class Player {
+
+    }
+    // Handle different enemy types
+    class Enemy {
+
+    }
+    // Handle individual background layers
+    class Layer {
+
+    }
+    // Animate the entire game's world
+    class Background {
+
+    }
+    // Draw score, timer and other information that needs to be displayed for the user
+    class UI {
+
+    }
+    // Handle the entire game
+    class Game {
+
+    }
 });
