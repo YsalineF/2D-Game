8e2c6db03c64d047184122872d40a200796f1821
Ysaline <ysaline.franquet@gmail.com>
2022-12-22 17:48:57 +0100

    Animating enemy sprite sheets
    Added the enemies' visual and animated it.

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ window.addEventListener('load', function(){
     // canvas setup
     const canvas = document.getElementById('canvas1');
     const ctx = canvas.getContext('2d');
-    canvas.width = 500;
+    canvas.width = 700;
     canvas.height = 500;
 
     // Keep track of specified user inputs (ex: arrow keys)
@@ -134,15 +134,21 @@ window.addEventListener('load', function(){
             this.markedForDeletion = false;
             this.lives = 5;
             this.score = this.lives;
+            this.frameX = 0;
+            this.frameY = 0;
+            this.maxFrame = 37;
         }
         update() {
-            this.x += this.speedX;
+            this.x += this.speedX - this.game.speed;
             if(this.x + this.width < 0) this.markedForDeletion = true;
+            // Sprite animation
+            if(this.frameX < this.maxFrame) {
+                this.frameX++;
+            } else this.frameX = 0;
         }
         draw(context) {
-            context.fillStyle = "red";
-            context.fillRect(this.x, this.y, this.width, this.height);
-            context.fillStyle = "black";
+            if(this.game.debug) context.strokeRect(this.x, this.y, this.width, this.height);
+            context.drawImage(this.image, this.frameX * this.width, this.frameY * this.height, this.width, this.height, this.x, this.y, this.width, this.height);
             context.font = '20px Helvetica';
             context.fillText(this.lives, this.x, this.y);
         }
@@ -154,9 +160,12 @@ window.addEventListener('load', function(){
         constructor(game) {
             // Refer to the parent constructor (super = parent)
             super(game);
-            this.width = 228 * 0.2;
-            this.height = 169 * 0.2;
+            this.width = 228;
+            this.height = 169;
             this.y = Math.random() * (this.game.height * 0.9 - this.height);
+            this.image = document.getElementById('angler1');
+            // Randomly choose a number between 1 and 3 bc we have 3 different animations
+            this.frameY = Math.floor(Math.random() * 3);
         }
     }
 
